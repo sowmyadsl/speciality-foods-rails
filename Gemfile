bc0501cead850ad124632a42eff7dbf3a7479736
sowmya dinavahi <sowmya.dsl@gmail.com>
2017-06-30 10:25:15 -0700

    add full crud functionality

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'rspec-rails'
 gem 'launchy'
 gem 'pry'
 gem 'shoulda-matchers'
+gem 'bootstrap-sass', '~> 3.3.6'
+
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,10 +17,30 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
-      redirect_to products_path
+      redirect_to @product
     else
       render :new
     end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @product= Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+   @product = Product.find(params[:id])
+   @product.destroy
+   redirect_to products_path
   end
 
   private
