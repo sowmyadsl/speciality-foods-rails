92f53a2c9fa9290b3e08fe1cc857d54852964fab
sowmya dinavahi <sowmya.dsl@gmail.com>
2017-06-30 14:00:51 -0700

    Add drop down menu for rating

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+
+50.times do |index|
+  new_product = Product.create!(name: Faker::Superhero.unique.power,
+                        price: Faker::Commerce.price,
+                        country: Faker::Address.country)
+  5.times do
+    new_product.reviews.create!(author: Faker::Superhero.name, content: Faker::Lorem.characters(100), rating: Faker::Number.between(1, 5))
+  end
+end
+
+p "Created #{Product.count} products"
+p "Created #{Review.count} reviews"
