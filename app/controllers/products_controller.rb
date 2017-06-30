a0f5e6c2fea4e73dfc7914128901908757f93ca1
sowmya dinavahi <sowmya.dsl@gmail.com>
2017-06-30 09:32:30 -0700

    Add functionality to create products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,23 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     render :show
   end
+
+  def new
+    @product = Product.new
+    render :new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :cost, :image, :country_of_origin)
+  end
 end
