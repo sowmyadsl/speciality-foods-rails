316104c4b06094ae771764b6ca899141e08925d3
sowmya dinavahi <sowmya.dsl@gmail.com>
2017-06-30 13:01:36 -0700

    Add association between product and review and added edit and destroy functionality

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -12,11 +12,35 @@ class ReviewsController < ApplicationController
     @product = Product.find(params[:product_id])
     @review = @product.reviews.new(review_params)
     if @review.save
+      flash[:notice] = "Review successfully created!"
       redirect_to product_path(@review.product)
     else
       render :new
     end
   end
+
+  def edit
+   @review = Review.find(params[:id])
+   @product = @review.product
+ end
+
+ def update
+   @review = Review.find(params[:id])
+   if @review.update(review_params)
+     flash[:notice] = "Review successfully updated!"
+     redirect_to product_path(@review.product)
+   else
+     render :edit
+   end
+ end
+
+ def destroy
+   @review = Review.find(params[:id])
+   if @review.destroy
+     flash[:notice] = "Review successfully removed!"
+    redirect_to product_path(@review.product)
+   end
+ end
 
   private
   def review_params
